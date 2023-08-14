9dd8e87480ce437a47de1376ec2b60df7f2f8c7d
danielfetico21 <109597145+danielfetico21@users.noreply.github.com>
2023-08-14 21:26:47 +0300

    save

--- app/settings.py
+++ app/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import os
 # config = Config()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,8 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+STATICFILES_DIRS = [ BASE_DIR, "static" ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
